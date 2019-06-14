5e8be7b1ad6134f99f294100c8c414df6be45d19
itsmarkodowd <godfather_mark@hotmail.com>
2019-06-14 18:35:08 +0100

    Updates

---
+++ precache-manifest.957fcfcb8f007540285ab2531ad8892a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afedc2c8a6eae52c39834492248efcd2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2d5b84257d8517a99c28",
+    "url": "/robofriends/static/css/main.f0d8d1b5.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "2d5b84257d8517a99c28",
+    "url": "/robofriends/static/js/main.99a4fd14.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
