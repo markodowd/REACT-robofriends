fe2a51b190009476eb3d9e78d09e3cbe9244682e
itsmarkodowd <godfather_mark@hotmail.com>
2019-10-17 02:26:50 +0100

    restructed redux files and removed logger middleware

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import { searchRobots, requestRobots } from './reducers';
-import './index.css';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+// import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import App from "./containers/App";
+import { searchRobots, requestRobots } from "./redux/reducers";
+import "./index.css";
+import "tachyons";
 
-const logger = createLogger();
+// const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobots, requestRobots});
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware
+    // logger
+  )
+);
 
 ReactDOM.render(
-		<Provider store={store}> 
-			<App />
-		</Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
